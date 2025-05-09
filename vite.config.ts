3becde908d2c127b546cffea9b1a18afa74c2bda
Ishaan Wadhwa <ishaanwadhwa.rkp@gmail.com>
2025-05-09 17:06:10 +0530

    Revert "gh-pages"
    This reverts commit c655788f4b5f49f5da509df6dcdb8685fd4c0a4e.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// replace 'your-repo-name' with your actual GitHub repository name
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
