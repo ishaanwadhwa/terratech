c655788f4b5f49f5da509df6dcdb8685fd4c0a4e
Ishaan Wadhwa <ishaanwadhwa.rkp@gmail.com>
2025-05-09 16:24:47 +0530

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// replace 'your-repo-name' with your actual GitHub repository name
 export default defineConfig({
+  base: '/terratech/',
   plugins: [react()],
   resolve: {
     alias: {
